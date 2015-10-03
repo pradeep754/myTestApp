c8f482aa62e5ed97b3b7289f2d67d1f95971ab4a
pradeep754 <kunaly7z7@gmail.com>
2015-10-03 15:45:00 +0530

    Update A.java

--- mygit/src/mygit/A.java
+++ mygit/src/mygit/A.java
@@ -2,11 +2,7 @@ package mygit;
 
 public class A {
 public static void main(String[] args) {
-<<<<<<< HEAD
-	//hzxdf
-=======
-	
->>>>>>> parent of ea003e1... dsdfd
+//this is okk
 	System.out.println("A");
 }
 }
