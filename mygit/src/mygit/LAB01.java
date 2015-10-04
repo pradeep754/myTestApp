599d62e5a32e3be1a70609bff6efce66ab0bf0c2
pradeep754 <kunaly7z7@gmail.com>
2015-10-04 15:32:43 +0530

    this is lab01

---
+++ mygit/src/mygit/LAB01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class LAB01 {
+		public static void main(String[] args) {
+	     	Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the String:");
+			String str=sc.next();
+			System.out.println(str);	
+
+
+	}
+
+}
